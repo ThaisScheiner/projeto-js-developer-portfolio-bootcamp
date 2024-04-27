0501cb8c0d167f0e79e66d83e06a2f37c3216b1c
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2024-04-26 21:09:43 -0300

    add api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = ''
+    const url = ' https://thaisscheiner.github.io/projeto-js-developer-portfolio-bootcamp/'
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
